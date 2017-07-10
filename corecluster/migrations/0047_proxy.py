75be2f1c370ebdf1d97fbfc942770c090fa34ece
Maciej Nabozny <maciej.nabozny@cloudover.io>
2017-07-10 10:20:52 +0200

    Fixed migration

--- corecluster/migrations/0047_proxy.py
+++ corecluster/migrations/0047_proxy.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('corecluster', '0048_merge'),
+        ('corecluster', '0046_migrate_hash'),
     ]
 
     operations = [
